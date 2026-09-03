58d6303901dc6710d1036f66f6edb5d292dbf5d0
Krish0176 <srivastavakrishna24@gmail.com>
2026-09-03 10:45:15 +0530

    Add various C++ source files including sorting algorithms, patterns, and linked list implementation

---
+++ linkedlist/intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Node{
+    int data;
+    Node *next;
+}
+
+
+int main() {
+    Node _first; //Static Node
+    // Your code goes here
+    Node *first = new Node(); //Dyanamic Node
+    return 0;
+}
