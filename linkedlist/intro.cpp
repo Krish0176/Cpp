bd64dea6b988ec294fceb52eacf1a2b9c5ed38b6
Krish0176 <srivastavakrishna24@gmail.com>
2026-09-04 14:14:05 +0530

    Update Node class to support string data type and modify main function accordingly

--- linkedlist/intro.cpp
+++ linkedlist/intro.cpp
@@ -3,10 +3,15 @@ using namespace std;
 class Node{
 public:
     int data;
+    string x;
     Node *next;
 
     Node(int value){
         this->data = value;
+        this->next = NULL;
+    }
+    Node(string k){
+        this->x = k;
         this->next = NULL;
     }
 };
@@ -14,14 +19,14 @@ public:
 
 int main() {
     Node *first = new Node(1); //Dyanamic Node
-    Node *second = new Node(2); 
+    Node *second = new Node("Hello"); 
     Node *third = new Node(3);
     first->next = second;
     second->next = third;
     third->next = NULL;
     
     cout << "FirstD: " << first->data << "\n";
-    cout << "secondD: " << second->data << "\n";
+    cout << "secondD: " << second->x<< "\n";
     cout << "thirdD: " << third->data << "\n";
 return 0;
 }
