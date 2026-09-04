a85ec67c41af8f9e21a0ba1b16983f77e0246358
Krish0176 <srivastavakrishna24@gmail.com>
2026-09-04 13:52:29 +0530

    Refactor code structure for improved readability and maintainability

--- linkedlist/intro.cpp
+++ linkedlist/intro.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 using namespace std;
 class Node{
+public:
     int data;
     Node *next;
-}
+
+    Node(int value){
+        this->data = value;
+        this->next = NULL;
+    }
+};
 
 
 int main() {
-    Node _first; //Static Node
-    // Your code goes here
-    Node *first = new Node(); //Dyanamic Node
-    return 0;
+    Node *first = new Node(1); //Dyanamic Node
+    Node *second = new Node(2); 
+    Node *third = new Node(3);
+    first->next = second;
+    second->next = third;
+    third->next = NULL;
+    
+    cout << "FirstD: " << first->data << "\n";
+    cout << "secondD: " << second->data << "\n";
+    cout << "thirdD: " << third->data << "\n";
+return 0;
 }
