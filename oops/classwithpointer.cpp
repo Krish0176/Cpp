7d217c8a496efa89bc089e446513a46fe992adc0
Krish0176 <srivastavakrishna24@gmail.com>
2026-09-23 11:17:42 +0530

    Implement structural updates and optimizations across multiple modules

---
+++ oops/classwithpointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Number{
+    int *p;
+    Number(int value){
+        p = new int(value);
+        
+    }
+};
+
+int main() {
+    
+    return 0;
+}
---
+++ stack&queue/introstack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+    int *arr;
+    int top;
+    int capacity;
+
+public:
+    Stack(int cap)
+    {
+        capacity = cap;
+        top = -1;
+        arr = new int[cap];
+    }
+
+    void push(int value)
+    {
+        if (top == capacity - 1)
+        {
+            cout << "Stack is already full!\n";
+            return;
+        }
+
+        top++;
+        arr[top] = value;
+        cout << "Stack was pushed successfully\n";
+    }
+
+    int peak()
+    {
+        if (top == -1)
+        {
+            cout << "Stack empty!";
+            return -1;
+        }
+        cout << "Current top elemnt= ";
+        return arr[top];
+    }
+
+    
+};
+
+int main()
+{
+    Stack s(3);
+
+    s.push(10);
+    s.push(20);
+    s.push(40);
+    s.push(50);
+
+    cout << s.peak();
+    return 0;
+}
