1406af0c8e9133332053436b9d2604b9f6c758f9
Krish0176 <srivastavakrishna24@gmail.com>
2026-09-26 23:31:08 +0530

    Refactor code structure for improved readability and maintainability

---
+++ linkedlist/operation.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
+};
+void insertatfirst(Node*& head,int value){
+    Node* newnode = new Node(value);
+    newnode->next = head;
+    head = newnode;
+}
+void insert(Node*& head,int value){
+    
+}
+void display(Node*& head){
+    Node* temp = head;
+    cout << "Linked list value: ";
+    while (temp != nullptr)
+    {
+        cout<< temp->data << " ";
+        temp = temp->next;
+        /* code */
+    }
+    
+    
+}
+int main() {
+    int n;
+    cout << "size of linked list: ";
+    cin >> n;
+    int arr[n];
+    cout << "array elements: ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+        Node* head = nullptr;
+
+    for(int i=0;i<n;i++){
+        insert(head,arr[i]);
+    }
+    //Node* newnode = new Node(value);
+    display(head);
+    
+    return 0;
+}
